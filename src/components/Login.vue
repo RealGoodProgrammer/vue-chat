e232e52d01add5b13e22fd6e5ea35c1181628090
Mic <realgoodprogrammer@gmail.com>
2018-05-27 20:53:51 +0300

    Added 'Login' component

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  v-container(fluid fill-height)
+    v-layout(align-center justify-center)
+      v-flex(xs12 sm8 md4)
+        v-card(class="elevation-12")
+          v-toolbar(dark color="secondary")
+            v-toolbar-title Авторизация
+
+          v-card-text(class="mt-4")
+              v-text-field(prepend-icon="person"
+                            v-model="name_field"
+                            label="Отображаемое имя"
+                            placeholder="Алексей")
+
+          v-card-actions
+            v-spacer
+            v-btn(color="secondary") Войти
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        name_field: null
+      }
+    }
+  }
+</script>
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
