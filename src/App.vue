9c36023cadeb7690ae5c0cb6b49cdc9d8e25da2f
Mic <realgoodprogrammer@gmail.com>
2018-05-27 19:46:56 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-app
+    v-content
+      router-view
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello Adamant!'
+    }
+  },
+  name: 'App'
+}
+</script>
+<style>
+
+</style>
