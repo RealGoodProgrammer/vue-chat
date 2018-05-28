7325ec656b8ee37b20090d75c7332e31cada3e94
Mic <realgoodprogrammer@gmail.com>
2018-05-28 10:09:37 +0300

    Routes were set up. Handled 404.

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-container(fluid fill-height)
+    v-layout(align-center justify-center)
+      v-flex(xs12 sm8 md4)
+        h1 404
+        h2 Такой страницы не существует
+        v-btn(@click="backToMain") Вернуться на главную
+</template>
+
+<script>
+  export default {
+    name: 'NotFoundComponent',
+    methods: {
+      backToMain () {
+        this.$router.push({name: 'Login'})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Chat from '@/components/Chat'
+import NotFoundComponent from '@/components/NotFoundComponent'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '*',
+      name: 'NotFoundComponent',
+      component: NotFoundComponent
+    },
     {
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat
     }
   ]
 })
